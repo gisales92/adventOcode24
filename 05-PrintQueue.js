8cd258f5474040775615b03264e14b168bdd1e36
Gregory Isales <isales.greg@gmail.com>
2024-12-24 13:42:25 -0800

    d5p2 complete

--- 05-PrintQueue.js
+++ 05-PrintQueue.js
@@ -1514,3 +1514,57 @@ inputUpdates.split("\n").forEach(update => {
 });
 
 console.log(count); // 6505
+
+/*
+--- Part Two ---
+While the Elves get to work printing the correctly-ordered updates, you have a little time to fix the rest of them.
+
+For each of the incorrectly-ordered updates, use the page ordering rules to put the page numbers in the right order. For the above example, here are the three incorrectly-ordered updates and their correct orderings:
+
+75,97,47,61,53 becomes 97,75,47,61,53.
+61,13,29 becomes 61,29,13.
+97,13,75,29,47 becomes 97,75,47,29,13.
+After taking only the incorrectly-ordered updates and ordering them correctly, their middle page numbers are 47, 29, and 47. Adding these together produces 123.
+
+Find the updates which are not in the correct order. What do you get if you add up the middle page numbers after correctly ordering just those updates?
+*/
+
+// helper function to fix a bad update
+const fixUpdate = (update) => {
+    let remaining = new Set(update);
+    let fixed = [];
+    while (remaining.size) {
+        for (let i = 0; i < update.length; i++) {
+            let page = update[i];
+            // if already added, let's continue
+            if (fixed.includes(page)) continue;
+            // if there's no overlap between the numbers that must be after this page and the remaining numbers
+            if (remaining.isDisjointFrom(ruleHash[page])) {
+                // we will build the fixed update from back to front
+                fixed.unshift(page);
+                // now that we've handled this page, we can remove from remaining set
+                remaining.delete(page);
+                // and keep going
+                break;
+            }
+        }
+    }
+    return fixed;
+};
+
+let fixedCount = 0;
+
+
+// parsing the updates from the input string
+inputUpdates.split("\n").forEach(update => {
+    let updateArr = update.split(",");
+    // checking update order
+    if (!checkUpdate(updateArr)) {
+        // if bad, fix order
+        const fixedUpdate = fixUpdate(updateArr);
+        // add midpoint val to counter
+        fixedCount += Number(fixedUpdate[(fixedUpdate.length - 1) / 2]);
+    }
+});
+
+console.log(fixedCount); // 6897
